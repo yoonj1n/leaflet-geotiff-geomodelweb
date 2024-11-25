bf999c91bb2711a9e6bd99feb56811637b06227f
yoonj1n <yoonj1n_@naver.com>
2024-11-25 14:25:09 +0900

    TESTME

--- src/leaflet-geotiff.js
+++ src/leaflet-geotiff.js
@@ -307,7 +307,8 @@ L.LeafletGeotiff = L.ImageOverlay.extend({
         return null;
 
       const i = y * this.raster.width + x;
-      const value = this.raster.data[0][i];
+      // const value = this.raster.data[0][i];
+      const value = this.raster.data[1][i];
 
       if (this.options.noDataValue === undefined) return value;
       const noData = parseInt(this.options.noDataValue);
