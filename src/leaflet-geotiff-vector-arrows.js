ead4247a2a0c44e445facb5fa003f48269d53ba3
yoonj1n <yoonj1n_@naver.com>
2024-11-26 14:37:07 +0900

    test : size set using zoom size

--- src/leaflet-geotiff-vector-arrows.js
+++ src/leaflet-geotiff-vector-arrows.js
@@ -93,7 +93,7 @@ L.LeafletGeotiff.VectorArrows = L.LeafletGeotiffRenderer.extend({
             ctx.lineTo(+arrowSize / 1.2, 0);
           }
           ctx.moveTo(arrowSize * 0.25, -arrowSize * 0.25);
-          ctx.lineTo(+arrowSize / 2, 0);
+          currentZoom <= 8? ctx.lineTo(+arrowSize / 2, 0) : ctx.lineTo(+arrowSize / 1.2, 0);
           ctx.lineTo(arrowSize * 0.25, arrowSize * 0.25);
           ctx.strokeStyle = color;
           ctx.stroke();
