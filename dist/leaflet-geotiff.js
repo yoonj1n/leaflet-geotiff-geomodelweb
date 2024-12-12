0f06fcbea56429309261ed8bbe28faff7069d6fc
yoonj1n <yoonj1n_@naver.com>
2024-12-13 01:06:34 +0900

    test

--- dist/leaflet-geotiff.js
+++ dist/leaflet-geotiff.js
@@ -250,7 +250,8 @@
         // invalid indices
         if (x < 0 || x > this.raster.width || y < 0 || y > this.raster.height) return null;
         const i = y * this.raster.width + x;
-        const value = this.raster.data[0][i];
+        // const value = this.raster.data[0][i];
+        const value = this.raster.data[1][i] ? this.raster.data[1][i] : this.raster.data[0][i];
         if (this.options.noDataValue === undefined) return value;
         const noData = parseInt(this.options.noDataValue);
         if (value !== noData) return value;
